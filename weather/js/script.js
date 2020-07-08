c1497d5edf405f18e6336e66f3671a6a2975231c
omkark45 <omkarak45@gmail.com>
2020-07-08 21:01:38 +0530

    Added some properties.

--- weather/js/script.js
+++ weather/js/script.js
@@ -11,7 +11,8 @@ const maxTempElement = document.querySelector('.max-temp-value');
 const humidityElement = document.querySelector('.humidity-pc');
 const windSpeedElement = document.querySelector('.wind-speed-pc');
 const precipitationElement = document.querySelector('.precipitation-pc');
-
+const dayElement = document.querySelector('.day');
+const container = document.querySelector('.container');
 const weather = {
     conditions:{},
     unit:'celsius',
@@ -27,6 +28,15 @@ var date = today.getDate();
 var month = today.getMonth();
 var year = today.getFullYear();
 var day = today.getDay();
+var time = today.getHours();
+if(time>18){
+  dayElement.innerHTML = `<p>Tonight</p>`;
+  container.classList.add('dark-sky');
+}else{
+  dayElement.innerHTML = `<p>Today</p>`;
+  container.classList.add('clear-sky');
+}
+console.log(time);
 const months = ['Jan','Feb','Mar','Apr','May','June','July','Aug','Sep','Oct','Nov','Dec'];
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 // Fetching user location
@@ -63,6 +73,7 @@ function showError(error) {
       break;
   }
 }
+// Get Weather Sub routine
 const KELVIN = 273.15;
 function getWeather(latitude, longitude) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a55396651e6cc7e680499099e0d3b9ab`;
@@ -76,22 +87,18 @@ function getWeather(latitude, longitude) {
       }
     })
     .then((data) => {
-        // console.log(data);
         weather.location = data.name;
         weather.country = data.sys.country;
         weather.description = data.weather[0].description;
         weather.iconId = data.weather[0].icon;
         weather.mainCondition = data.weather[0].main;
-        // all temps in kelvin
         weather.temperature = data.main.temp;
         weather.feels_like = data.main.feels_like;
         weather.temp_min = data.main.temp_min;
         weather.temp_max = data.main.temp_max;
         weather.conditions.pressure = data.main.pressure;
         weather.conditions.humidity = data.main.humidity;
-        // wind speed unit is meter/sec
         weather.conditions.wind_speed = data.wind.speed;
-        // wind direction is in degrees
         weather.conditions.wind_speed_deg = data.wind.deg;
         
         console.log(weather);
@@ -103,17 +110,16 @@ function getWeather(latitude, longitude) {
 // Changing the html dom !
 function displayWeather(){
     // 1. Changing Location HTML
-    locationElement.innerHTML = `<h2>${weather.location},</h2>`
-    countryElement.innerHTML = `<h2>${weather.country}</h2>`
-    dateElement.innerHTML = `<p>${months[month]},${date} ${days[day]}</p>`
-    tempElement.textContent = `${Math.floor(weather.temperature-KELVIN)} °C`;
-    
-    iconElement.innerHTML = `<img src='icons/icons/${weather.iconId}.png'>`
-    descElement.innerHTML = `<p>${weather.description}</p>`;
-    minTempElement.innerHTML = `<p>${Math.floor(weather.temp_min-KELVIN)}°C</p>`
-    maxTempElement.innerHTML = `<p>${Math.floor(weather.temp_max-KELVIN)}°C</p>`
-    humidityElement.innerHTML = `<p>${weather.conditions.humidity} %</p>`
-    windSpeedElement.innerHTML = `<p>${weather.conditions.wind_speed} m/s</p>`;
+    locationElement.innerHTML   = `<h2>${weather.location},</h2>`
+    countryElement.innerHTML    = `<h2>${weather.country}</h2>`
+    dateElement.innerHTML       = `<p>${months[month]},${date} ${days[day]}</p>`
+    tempElement.textContent     = `${Math.floor(weather.temperature-KELVIN)} °C`;
+    iconElement.innerHTML       = `<img src='icons/icons/${weather.iconId}.png'>`
+    descElement.innerHTML       = `<p>${weather.description}</p>`;
+    minTempElement.innerHTML    = `<p>${Math.floor(weather.temp_min-KELVIN)}°C</p>`
+    maxTempElement.innerHTML    = `<p>${Math.floor(weather.temp_max-KELVIN)}°C</p>`
+    humidityElement.innerHTML   = `<p>${weather.conditions.humidity} %</p>`
+    windSpeedElement.innerHTML  = `<p>${weather.conditions.wind_speed} m/s</p>`;
     precipitationElement.innerHTML = `<p>${weather.conditions.pressure} atm</p>`
 }
 
@@ -122,7 +128,6 @@ function CtoF(temperature){
 }
 
 tempElement.addEventListener('click',function(){
-    // console.log('you clicked me');
     if(weather.unit == 'celsius'){
         let fahrenheit = CtoF(weather.temperature-KELVIN);
         fahrenheit = Math.floor(fahrenheit);  
