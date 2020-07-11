1d56037836fdde35b177d328d3a2217cc972dfdc
omkark45 <omkarak45@gmail.com>
2020-07-11 19:05:42 +0530

    Added Dark mode classes

--- weather/js/darkMode.js
+++ weather/js/darkMode.js
@@ -1,11 +1,15 @@
 // Selecting DOM - Dark Mode Toggle 
+
 const darkBtn = document.querySelector('.night-mode-toggle');
 const navBar = document.querySelector('.title');
 const darkBtnIcon = document.getElementById('darkModeIcon');
 const bodyElement = document.getElementById('body');
 const menuDark = document.querySelector('.nav');
+const humidityElementDark = document.querySelector('.humidity');
+const windSpeedElementDark = document.querySelector('.wind-speed');
+const precipitationElementDark = document.querySelector('.precipitation');
+// Calling Dark Mode When Clicked 
 darkBtn.addEventListener('click',function(){
-    
     darkMode();
 })
 darkBtn.addEventListener('click',function(){
@@ -22,5 +26,6 @@ darkBtn.addEventListener('click',function(){
 function darkMode(){
     navBar.classList.toggle('title-dark');
     bodyElement.classList.toggle('body-dark');
-    menuDark.classList.toggle('show-menu-dark')
+    menuDark.classList.toggle('show-menu-dark');
+    humidityElementDark.classList.add('humidity-dark')
 }
