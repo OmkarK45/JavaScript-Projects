3294d59653f69236ab5dc9e85061f5c9024ec54c
omkark45 <omkarak45@gmail.com>
2020-07-15 18:55:34 +0530

    Added subroutine for darkMode and LightMode

--- dark-mode/js/app.js
+++ dark-mode/js/app.js
@@ -1,14 +1,46 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
-// Function
-function switchTheme(event){
-    console.log(event.target.checked);
-    if(event.target.checked){
-        document.documentElement.setAttribute('data-theme','dark');
-    }else{
-        document.documentElement.setAttribute('data-theme','light');
-    }
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
+
+// Dark Mode Function
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0,0,0,0.5)";
+  textBox.style.backgroundColor = "rgb(255,255,255,0.5)";
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.remove("fa-sun");
+  toggleIcon.children[1].classList.add("fa-moon");
+  image1.src = "src/img/undraw_feeling_proud_dark.svg";
+  image2.src = "src/img/undraw_feeling_proud_dark.svg";
+  image3.src = "src/img/undraw_feeling_proud_dark.svg";
 }
 
+// Light Mode Styles
+function lightMode() {
+  nav.style.backgroundColor = "rgb(255,255,255,0.5)";
+  textBox.style.backgroundColor = "rgb(0,0,0,0.5)";
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.remove("fa-moon");
+  toggleIcon.children[1].classList.add("fa-sun");
+  image1.src = "src/img/undraw_feeling_proud_light.svg";
+  image2.src = "src/img/undraw_feeling_proud_light.svg";
+  image3.src = "src/img/undraw_feeling_proud_light.svg";
+}
+
+// Function
+function switchTheme(event) {
+  console.log(event.target.checked);
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    darkMode();
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    lightMode();
+  }
+}
 
 // Event Listener
-toggleSwitch.addEventListener('change',switchTheme);
+toggleSwitch.addEventListener("change", switchTheme);
