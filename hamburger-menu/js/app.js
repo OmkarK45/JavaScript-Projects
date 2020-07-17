b705f33c6811f015dcad5429c2c89a3ea5e80fc5
omkark45 <omkarak45@gmail.com>
2020-07-17 17:18:01 +0530

    Multiple Changes

---
+++ hamburger-menu/js/app.js
@@ -0,0 +1,5 @@
+const toggleButton = document.querySelector('.toggle-menu');
+const navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click',()=>{
+    navBar.classList.toggle('toggle');
+})
