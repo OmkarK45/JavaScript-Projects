db9131745d4b6e0eac66ba438019304ba7161800
omkark45 <omkarak45@gmail.com>
2020-07-20 23:17:07 +0530

    sadfasdf

--- media-player/js/app.js
+++ media-player/js/app.js
@@ -1,97 +1,106 @@
-const image = document.querySelector('img');
-const title = document.getElementById('title');
-const artist = document.getElementById('artist');
-const music = document.querySelector('audio');
-const prevBtn = document.getElementById('prev');
-const playBtn = document.getElementById('play');
-const nextBtn = document.getElementById('next');
-
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const music = document.querySelector("audio");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
+const currentTimeEl = doc
 // Music
 const songs = [
-    {
-        name:'lean on',
-        displayName: 'Lean On',
-        artist:'Major Lazer ft. MØ & DJ Snake',
-    },
-    {
-        name:'starboy',
-        displayName: 'Starboy ft. Daft Punk',
-        artist:'The Weeknd',
-    },
-    {
-        name:'blinding lights',
-        displayName: 'Blinding Lights',
-        artist:'The Weeknd',
-    },
-    {
-        name:'take me home',
-        displayName: 'Take Me Home',
-        artist:'Aero Chord',
-    },
-    
+  {
+    name: "lean on",
+    displayName: "Lean On",
+    artist: "Major Lazer ft. MØ & DJ Snake",
+  },
+  {
+    name: "starboy",
+    displayName: "Starboy ft. Daft Punk",
+    artist: "The Weeknd",
+  },
+  {
+    name: "blinding lights",
+    displayName: "Blinding Lights",
+    artist: "The Weeknd",
+  },
+  {
+    name: "take me home",
+    displayName: "Take Me Home",
+    artist: "Aero Chord",
+  },
 ];
 
-
-
-// Check if playing 
+// Check if playing
 let isPlaying = false;
 
-
-
-// Play 
-function playSong(){
-    isPlaying = true;
-    playBtn.classList.replace('fa-play','fa-pause')
-    music.play();
+// Play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  music.play();
 }
 
 // Pause
-function pauseSong(){
-    isPlaying = false;
-    playBtn.classList.replace('fa-pause','fa-play')
-    music.pause();
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  music.pause();
 }
 
 // Play or Pause button listener
-playBtn.addEventListener('click',() => (isPlaying ? pauseSong() : playSong()));
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
 
 // Update DOM
-function loadSong(song){
-    title.textContent = song.displayName;
-    artist.textContent = song.artist;
-    music.src = `src/music/${song.name}.flac`;
-    image.src = `src/img/${song.name}.png`;
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `src/music/${song.name}.flac`;
+  image.src = `src/img/${song.name}.png`;
 }
 
 // Current song
 let songIndex = 0;
 
 // Previous Song
-function prevSong(){
-    songIndex--;
-    if(songIndex < 0){
-        songIndex = songs.length - 1;
-    }
-    console.log(songIndex);
-    loadSong(songs[songIndex]);
-    playSong();
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  console.log(songIndex);
+  loadSong(songs[songIndex]);
+  playSong();
 }
 // Next Song
-function nextSong(){
-    songIndex++;
-    if(songIndex>songs.length-1){
-        songIndex = 0;
-    }
-    console.log(songIndex);
-    loadSong(songs[songIndex]);
-    playSong();
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  console.log(songIndex);
+  loadSong(songs[songIndex]);
+  playSong();
 }
 
-// On load - Select first song 
+// On load - Select first song
 loadSong(songs[songIndex]);
 
+// Update Progress bar and time
+function updateProgressBar(event) {
+  if (isPlaying) {
+    const { duration, currentTime } = event.srcElement;
+    // console.log(duration, currentTime);
+    // Update progress bar width
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+}
 // Event Listener for forwards and backward
-prevBtn.addEventListener('click', prevSong);
-nextBtn.addEventListener('click', nextSong);
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
 
-// Make BG React to Album art colors 
+// Make BG React to Album art colors
+// Event
+music.addEventListener("timeupdate", updateProgressBar);
