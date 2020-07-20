e188cc07655ce831906a514bd9c8f5117dc852a4
omkark45 <omkarak45@gmail.com>
2020-07-20 20:43:10 +0530

    Editied about section. Fixed Hammy :rocket:

--- hamburger-menu/js/app.js
+++ hamburger-menu/js/app.js
@@ -1,5 +1,12 @@
 const toggleButton = document.querySelector('.toggle-menu');
 const navBar = document.querySelector('.nav-bar');
+const navLink  = document.querySelectorAll('.nav-link');
 toggleButton.addEventListener('click',()=>{
     navBar.classList.toggle('toggle');
-})
+});
+navLink.forEach(item=>{
+    item.addEventListener('click',()=>{
+        console.log('clicked a menu link !');
+        navBar.classList.remove('toggle');
+    });
+});
