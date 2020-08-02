6d85547bc7f383ea7de55d372a53b990b2e49308
omkark45 <omkarak45@gmail.com>
2020-08-02 15:13:47 +0530

    Modal implemented.

--- TypeR/js/engine.js
+++ TypeR/js/engine.js
@@ -16,10 +16,17 @@ wordsArr = words.split(" ");
 let randomIndex = 0;
 let score = 0;
 let highScore = 0;
+let timerStarted  = false;
 // MAIN DRIVER FUNCTION
 function myFunction(event){
     var x = event.which || event.keyCode;
     if(x==32){
+        // Start the countdown and change flag to true
+        if(!timerStarted){
+            console.log('i will start timer');
+            countdown();
+            timerStarted = true;
+        }
         document.body.onkeyup = function(e){
             if((e.keyCode == 32||e.keyCode==13) && !isMisspelled){
                 textTyped.focus();
@@ -81,7 +88,6 @@ function highScoreKeeper(scoreValue){
 }
 // Event listener on start button 
 startBtn.addEventListener('click', function(){
-    countdown();
     resetGame();
 });
 
@@ -98,11 +104,40 @@ function countdown() {
             let wpm=0;
             wpm = Math.floor((score/60)*100);
             alert(`Your score is ${wpm}`);
+            timerStarted = false;
         }
     }
     tick();
 }
 
+
+// Modal Logic 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  console.log('modal triggered')
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 // Hmm logic ;; add event listener on space key  -- done 
 // for every correct word user types, increment score by one -- done  
