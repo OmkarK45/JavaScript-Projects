952abd8d0b4843bb4299114ba88039658083522a
omkark45 <omkarak45@gmail.com>
2020-08-03 13:28:14 +0530

    Close button for modal added

--- TypeR/js/engine.js
+++ TypeR/js/engine.js
@@ -106,6 +106,10 @@ function countdown() {
             timerStarted = false;
             speedWPMElement.textContent = `${wpm}`;
             modal.style.display = "block";
+            span.onclick = function() {
+                modal.style.display = "none";
+              }
+              
         }
     }
     tick();
@@ -129,9 +133,6 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
